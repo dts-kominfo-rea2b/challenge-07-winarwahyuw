a3402b62688be51ae7b1bdcc63e101ac69e45ba2
winarwahyuw <winarwahyuw99@gmail.com>
2022-06-10 16:11:22 +0700

    test passed tapi tidak yakin benar

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let IXX = await promiseTheaterIXX();
+  let VGC = await promiseTheaterVGC();
+  
+  return new Promise((resolve)=>{
+      let countIXX = IXX.filter((item)=>item.hasil == emosi).length;
+      let countVGC = VGC.filter((item)=>item.hasil == emosi).length;
+
+      resolve(countIXX + countVGC);
+  })
+};
+
 
 module.exports = {
   promiseOutput,
